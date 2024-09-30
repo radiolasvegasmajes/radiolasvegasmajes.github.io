ac5b8fde33b3d07f644139ce4e4501a6218915a4
radiolasvegasmajes <150864335+radiolasvegasmajes@users.noreply.github.com>
2024-09-30 17:59:12 -0500

    Add files via upload

---
+++ player/serviceworker.js
@@ -0,0 +1,37 @@
+const cachea = "HVHCache-v1";
+var appshellfiles = [
+    '/index.html',
+    '/home.html',
+    '/counterPage.html',
+    '/github.html',
+    '/info.html',
+    '/tab.min.js',
+    '/save.min.css',
+    '/manifest.json',
+    '/logo.png',
+    'fonts/MatSymbolsRounded.woff2',
+    'fonts/Poppins-Medium.ttf'
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil((async () => {
+        const cache = await caches.open(cachea);
+        await cache.addAll(appshellfiles);
+    })());
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+});
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    return fetch(request);
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
+});
